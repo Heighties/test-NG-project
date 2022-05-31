0fa5a5b0dac04337c840449acc0fdf115a6745b0
Heighties <heighties@gmail.com>
2022-05-31 11:24:06 +0200

    start responsive

--- examples/examples.js
+++ examples/examples.js
@@ -41,3 +41,19 @@ window.onload = () => {
 
 }
 
+
+//HAMBURGER MENU
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+       hamburger.classList.remove("active");
+       menu.classList.remove("active");  
+    }))
